d2adf72ccaa4b2719cd5c2f9e37f98532a3df202
installer <installer@gmail.com>
2025-05-05 20:05:58 +0200

    Remove obsolete subfolder and finalize structure

--- wretched-wavelength/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://eddinesu.github.io/EddineSU-Portfolio/",  // Remplacé par l'URL de ton site GitHub Pages
-  base: "/EddineSU-Portfolio/",  // Remplacé par le nom de ton dépôt GitHub
-});
