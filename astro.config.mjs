30db00f7fa6e8aecf412c0444bfb05bbe40e9cbc
Maximilian NEU <neu.maximilian@proton.me>
2025-06-09 11:03:48 +0200

    Revert "Update configuration and fix URL paths in project components"
    This reverts commit 1f0bdae912329330615ff57225f298dba94f7b71.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://eddinesu.github.io',
-	base: '/EddineSU-Portfolio',
 	integrations: [tailwind()],
 });
